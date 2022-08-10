12c30322a5b31e21118b51aeac900ced667cc855
Nathanael Valentino Davis <nathanael.vd@gmail.com>
2022-08-10 16:13:33 +0700

    add new users endpoint

--- server.go
+++ server.go
@@ -11,5 +11,30 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.GET("/users/", func(c echo.Context) error {
+		user := []map[string]interface{}{
+			{
+				"name": "Harry Potter",
+				"city": "London",
+			},
+			{
+				"name": "Don Quixote",
+				"city": "Madrid",
+			},
+			{
+				"name": "Joan of Arc",
+				"city": "Paris",
+			},
+			{
+				"name": "Rosa Park",
+				"city": "Alabama",
+			},
+		}
+
+		return c.JSON(http.StatusOK, user)
+
+	})
+
 	e.Logger.Fatal(e.Start(":3000"))
 }
